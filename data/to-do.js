8bdb3d779665d22255a892a21b271cd4de228855
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-23 14:34:31 -0700

    added todo seed data, checking get route works to grab data

---
+++ data/to-do.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    to_do: 'Call apartment office',
+    completed: true,
+    user_id: 2
+  },
+
+  {
+    to_do: 'Call Portland Gas and Electric company',
+    completed: false,
+    user_id: 2
+  },
+
+  {
+    to_do: 'Call Comcast',
+    completed: false,
+    user_id: 2
+  },
+
+  {
+    to_do: 'Car Wash',
+    completed: true,
+    user_id: 2
+  },
+
+  {
+    to_do: 'Finish packing',
+    completed: false,
+    user_id: 2
+  }
+];
