25b55ef5a4bcba5b9df60eab7581d2828be1b440
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-23 17:31:49 -0700

    put test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -68,14 +68,15 @@ describe('app routes', () => {
       ];
 
       const data = await fakeRequest(app)
-        .get('/todos')
+        .get('/api/todos')
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
     });
 
-    test('POST /todos creates a new todo', async() => {
+    test('POST /api/todos creates a new todo', async() => {
       const newTodo = {
         to_do: 'Feed Latte',
         completed: true,
@@ -83,12 +84,30 @@ describe('app routes', () => {
       };
 
       const todoData = await fakeRequest(app)
-        .post('/todos')
+        .post('/api/todos')
+        .set('Authorization', token)
         .send(newTodo)
         .expect(200)
         .expect('Content-type', /json/);
       expect(todoData.body.to_do).toEqual(newTodo.to_do);
       expect(todoData.body.id).toBeGreaterThan(0);
+    });
+
+    test('PUT /api/todos/:id updates a todos', async() => {
+      const updateTodo = {
+        id: 1,
+        to_do: 'Call apartment office',
+        completed: false,
+        user_id: 2
+      };
+      const todoData = await fakeRequest(app)
+        .put('/api/todos/1')
+        .set('Authorization', token)
+        .send(updateTodo)
+        .expect(200)
+        .expect('Content-Type', /json/);
+      expect(todoData.body.completed).toEqual(updateTodo.completed);
+      
     });
   });
 });
